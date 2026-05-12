90844a83da28d9a2a27d74bc19c93cdef1fb66af
talimirchuk <talii.mirr@gmail.com>
2026-05-12 12:04:29 -0300

    Termine Consigna Base:)

--- main.js
+++ main.js
@@ -9,5 +9,33 @@ Distancia	  | Con SUBE registrada   | Con SUBE sin registrar
 */
 
 boton.addEventListener("click", () => {
-  // COMPLETAR
+  let RegistradaValor =  registrada.value;
+  let DistanciaValor = Number(distancia.value);
+  if (DistanciaValor >= 0 && DistanciaValor <= 3) {
+    if (RegistradaValor === "si") {
+      tarifa.innerText = "$715.24";
+    } else {
+      tarifa.innerText = "$1137.23";
+    }
+  } else if (DistanciaValor > 3 && DistanciaValor <= 6) {
+    if (RegistradaValor === "si") {
+      tarifa.innerText = "$794.74";
+    } else {
+      tarifa.innerText = "$1263.64";
+    }
+  } else if (DistanciaValor > 6 && DistanciaValor <= 12) {
+    if (RegistradaValor === "si") {
+      tarifa.innerText = "$855.97";
+    } else {
+      tarifa.innerText = "$1360.99";
+    }
+  } else if (DistanciaValor > 12 && DistanciaValor <= 27) {
+    if (RegistradaValor === "si") {
+      tarifa.innerText = "$917.24";
+    } else {
+      tarifa.innerText = "$1458.41";
+    }
+  }
 });
+
+
